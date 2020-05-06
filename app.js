26103716a279409baa19c37049c35d235eca6bfc
BpAPP <b.papp@warwick.ac.uk>
2020-05-06 22:01:31 +0100

    Added basic boilerplate for HTTP and HTTPS Add packages: ejs body-parser method-overwrite mongoose

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-//test
-console.log("ok");
-console.log("ok2")
+//IMPORTS & SETUP
+const express = require("express");
+const app = express();
+
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+const options={
+	key: fs.readFileSync('./certificates/domain.key'),
+	cert: fs.readFileSync("./certificates/domain.crt")
+};
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+var mongoose    = require("mongoose");
+mongoose.connect("mongodb://localhost/blog",{useNewUrlParser:true, useUnifiedTopology:true});
+
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+//ROUTES
+app.get("/",function(req,res){
+    res.send("This is a message from the server.");
+});
+
+https.createServer(options,app).listen(3000,function(){
+    console.log("HTTPS server listening on port 3000");
+});
+http.createServer(app).listen(3001,function(){
+    console.log("HTTP server listening on port 3001");
+});
